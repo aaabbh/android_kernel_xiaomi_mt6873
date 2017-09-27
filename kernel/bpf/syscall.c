35d30664b5d9895b432700e20160631471534700
Martin KaFai Lau <kafai@fb.com>
2017-09-27 14:37:53 -0700

    BACKPORT: bpf: Add map_name to bpf_map_info
    This patch allows userspace to specify a name for a map
    during BPF_MAP_CREATE.
    
    The map's name can later be exported to user space
    via BPF_OBJ_GET_INFO_BY_FD.
    
    Change-Id: I96b8d74b09c14f2413d421bba61cfa63d1730bc3
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Alexei Starovoitov <ast@fb.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Tkpointz <pointzztk@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>
    
    0

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -382,8 +382,7 @@ static int bpf_obj_name_cpy(char *dst, const char *src)
 
 	return 0;
 }
-#define BPF_MAP_CREATE_LAST_FIELD map_extra
-
+#define BPF_MAP_CREATE_LAST_FIELD map_name
 /* called via syscall */
 static int map_create(union bpf_attr *attr)
 {
