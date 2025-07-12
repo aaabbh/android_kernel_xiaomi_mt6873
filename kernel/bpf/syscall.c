561be3f2811cef3ff064c169113f362d9f89cb10
aaabbh <2268951925@qq.com>
2025-07-13 06:58:18 +0800

    Add files via upload

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -377,10 +377,13 @@ static int bpf_obj_name_cpy(char *dst, const char *src)
 	if (src == end)
 		return -EINVAL;
 
+	/* '\0' terminates dst */
+	*dst = 0;
+
 	return 0;
 }
 
-#define BPF_MAP_CREATE_LAST_FIELD map_name
+#define BPF_PROG_LOAD_LAST_FIELD prog_name
 /* called via syscall */
 static int map_create(union bpf_attr *attr)
 {
@@ -1171,8 +1174,7 @@ static int bpf_prog_attach_check_attach_type(const struct bpf_prog *prog,
 }
 
 /* last field in 'union bpf_attr' used by this command */
-#define	BPF_PROG_LOAD_LAST_FIELD expected_attach_type
-
+#define	BPF_PROG_LOAD_LAST_FIELD prog_name
 static int bpf_prog_load(union bpf_attr *attr)
 {
 	enum bpf_prog_type type = attr->prog_type;
@@ -1678,7 +1680,11 @@ static int bpf_prog_get_info_by_fd(struct bpf_prog *prog,
 	info.nr_map_ids = prog->aux->used_map_cnt;
 	ulen = min_t(u32, info.nr_map_ids, ulen);
 	if (ulen) {
+<<<<<<< HEAD
 		u32 __user *user_map_ids = u64_to_user_ptr(info.map_ids);
+=======
+		u32 *user_map_ids = (u32 *)info.map_ids;
+>>>>>>> a2b662dced04 (BACKPORT: bpf: Add name, load_time, uid and map_ids to bpf_prog_info)
 		u32 i;
 
 		for (i = 0; i < ulen; i++)
